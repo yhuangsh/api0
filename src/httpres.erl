1d22cc44b6a332c7752a87d0d29977874fa9b2a9
Yong Huang <yong.huang.sh@gmail.com>
2019-01-13 23:51:42 +0800

    Change default respone content-type to text/json

--- src/httpres.erl
+++ src/httpres.erl
@@ -3,7 +3,8 @@
 -export(['200'/2, '200'/3,
          '400'/3, '404'/3]).
 
--define(CT_TEXT, <<"content-type">> => <<"text/text">>).
+-define(CT_TEXT_TEXT, <<"text/text">>).
+-define(CT_TEXT_JSON, <<"text/json">>).
 
 %%====================================================================
 %% API
@@ -20,4 +21,5 @@
 %%====================================================================
 
 reply(C, R, S) -> {ok, cowboy_req:reply(C, R), S}.
-reply(C, T, R, S) -> {ok, cowboy_req:reply(C, #{?CT_TEXT}, T, R), S}.
+reply(C, T, R, S) -> reply(C, ?CT_TEXT_JSON, T, R, S).
+reply(C, CT, T, R, S) -> {ok, cowbot_req:reply(C, #{<<"content-type">> => CT}, T, R), S}.
