7501d4566b8a26e71a8a9621d2a817d01d27fadd
Yong Huang <yong.huang.sh@gmail.com>
2019-01-15 00:46:55 +0800

    Clean up httpres

--- src/httpres.erl
+++ src/httpres.erl
@@ -1,10 +1,10 @@
 -module(httpres).
 
--export(['200'/2, '200'/3,
-         '400'/3, '404'/3]).
+-export(['200'/2, '200'/3, '200'/4,
+         '404'/2, '404'/3, '404'/4]).
 
 -define(CT_TEXT_TEXT, <<"text/text">>).
--define(CT_TEXT_JSON, <<"text/json">>).
+-define(CT_APPLICATION_JSON, <<"application/json">>).
 
 %%====================================================================
 %% API
@@ -12,14 +12,16 @@
 
 '200'(R, S) -> reply(200, R, S).
 '200'(T, R, S) -> reply(200, T, R, S).
+'200'(json, T, R, S) -> reply(200, ?CT_APPLICATION_JSON, T, R, S).
 
-'400'(T, R, S) -> reply(400, T, R, S).
+'404'(R, S) -> reply(404, R, S).
 '404'(T, R, S) -> reply(404, T, R, S).
+'404'(json, T, R, S) -> reply(404, ?CT_APPLICATION_JSON, T, R, S).
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
 
 reply(C, R, S) -> {ok, cowboy_req:reply(C, R), S}.
-reply(C, T, R, S) -> reply(C, ?CT_TEXT_JSON, T, R, S).
+reply(C, T, R, S) -> reply(C, ?CT_TEXT_TEXT, T, R, S).
 reply(C, CT, T, R, S) -> {ok, cowboy_req:reply(C, #{<<"content-type">> => CT}, T, R), S}.
