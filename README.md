57151f3b09dc5d9ba97f7a013ee45802d06f245f
Yong Huang <yong.huang.sh@gmail.com>
2019-01-15 21:09:23 +0800

    Complete disc backed mnesia node initialization

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 
 ## Tips
 
-Looks like after you've created a new builder, you need to at least manually build once in order for Jenkins to automatically install the Github webhook for you.
+- Looks like after you've created a new builder, you need to at least manually build once in order for Jenkins to automatically install the Github webhook for you.
+
+- Mnesia directory must exists for schema to be created as disc_copies type.
--- src/api0_main.erl
+++ src/api0_main.erl
@@ -20,9 +20,8 @@ start_link() ->
 
 init(S) ->
     Np = connect_prev_node(node()),
-    ok = mnesia:start(),
-    {atomic, ok} = start_mnesia(Np),
-    start_cowboy(S), 
+    {atomic, ok} = start_mnesia_init_tab(Np),
+    {ok, _} = start_cowboy(S), 
     {ok, S}.
 
 handle_call(_Cmd, _From, State) ->
@@ -46,11 +45,24 @@ connect_prev_node(N) when is_integer(N) ->
     pong = net_adm:ping(Np1),
     Np1.
 
-%%
-start_mnesia(none) -> {atomic, ok} = mnesia:create_table(?USER_TAB, []);
-start_mnesia(Np) when is_atom(Np) ->
+%% 
+start_mnesia_init_tab(none) -> 
+    CreateSchemaRet = mnesia:create_schema([node()]),
+    mnesia:start(),
+    init_tab_0(CreateSchemaRet);
+start_mnesia_init_tab(Np) when is_atom(Np) ->
+    ok = mnesia:start(),
+    SchemaStorageType = mnesia:table_info(schema, storage_type),
+    init_tab_n(SchemaStorageType, Np).
+
+init_tab_0(ok) -> {atomic, ok} = mnesia:create_table(?USER_TAB, [{disc_copies, [node()]}]);
+init_tab_0({error, {_,{already_exists, _}}}) -> {atomic, ok}.
+
+init_tab_n(ram_copies, Np) -> 
     {ok, _} = mnesia:change_config(extra_db_nodes, [Np]),
-    {atomic, ok} = mnesia:add_table_copy(?USER_TAB, node(), ram_copies).
+    {atomic, ok} = mnesia:change_table_copy_type(schema, node(), disc_copies),
+    {atomic, ok} = mnesia:add_table_copy(?USER_TAB, node(), disc_copies);
+init_tab_n(disc_copies, _) -> {atomic, ok}.
 
 %%
 start_cowboy(S) ->
