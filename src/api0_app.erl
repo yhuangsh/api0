98ac79dec400a23d0c4d2f107bd89a8f06de1737
Yong Huang <yong.huang.sh@gmail.com>
2019-01-16 00:06:14 +0800

    Fix a node start error where a node failed and all its data gone, then back up again.

--- src/api0_app.erl
+++ src/api0_app.erl
@@ -47,21 +47,25 @@ connect_prev_node(N) when is_integer(N) ->
 %% 
 start_mnesia_init_tab(none) -> 
     CreateSchemaRet = mnesia:create_schema([node()]),
-    mnesia:start(),
+    ok = mnesia:start(),
     init_tab_0(CreateSchemaRet);
 start_mnesia_init_tab(Np) when is_atom(Np) ->
     ok = mnesia:start(),
-    SchemaStorageType = mnesia:table_info(schema, storage_type),
-    init_tab_n(SchemaStorageType, Np).
+    SchemaStorageType0 = mnesia:table_info(schema, storage_type),
+    add_this_node(SchemaStorageType0, Np),
+    SchemaStorageType1 = mnesia:table_info(schema, storage_type),
+    init_tab_n(SchemaStorageType1).
 
 init_tab_0(ok) -> {atomic, ok} = mnesia:create_table(?USER_TAB, [{disc_copies, [node()]}]);
 init_tab_0({error, {_,{already_exists, _}}}) -> {atomic, ok}.
 
-init_tab_n(ram_copies, Np) -> 
-    {ok, _} = mnesia:change_config(extra_db_nodes, [Np]),
+add_this_node(ram_copies, Np) -> {ok, _} = mnesia:change_config(extra_db_nodes, [Np]);
+add_this_node(disc_copies, _) -> {ok, ignored}.
+
+init_tab_n(ram_copies) -> 
     {atomic, ok} = mnesia:change_table_copy_type(schema, node(), disc_copies),
     {atomic, ok} = mnesia:add_table_copy(?USER_TAB, node(), disc_copies);
-init_tab_n(disc_copies, _) -> {atomic, ok}.
+init_tab_n(disc_copies) -> {atomic, ok}.
 
 %%
 start_cowboy(S) ->
