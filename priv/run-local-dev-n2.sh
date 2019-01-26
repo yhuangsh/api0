0920fa65c43896e15211bb70f327bcbde17eb39a
Yong Huang <yong.huang.sh@gmail.com>
2019-01-26 13:38:46 +0800

    Fix wrong ip address

--- priv/run-local-dev-n2.sh
+++ priv/run-local-dev-n2.sh
@@ -3,11 +3,11 @@ docker run \
     --rm \
     --net dev-net \
     --name api0-2 \
-    --ip 172.28.0.12 \
+    --ip 172.28.0.14 \
     --hostname api0-2.api0.default.svc.cluster.local \
-    --add-host api0-0.api0.default.svc.cluster.local:172.29.0.2 \
-    --add-host api0-1.api0.default.svc.cluster.local:172.29.0.3 \
-    --add-host api0-2.api0.default.svc.cluster.local:172.29.0.4 \
+    --add-host api0-0.api0.default.svc.cluster.local:172.28.0.12 \
+    --add-host api0-1.api0.default.svc.cluster.local:172.28.0.13 \
+    --add-host api0-2.api0.default.svc.cluster.local:172.28.0.14 \
     -v `pwd`:/project \
     -p 8002:8000 \
     -it \
--- priv/run-local-dev-test.sh
+++ priv/run-local-dev-test.sh
@@ -2,13 +2,12 @@
 docker run \
     --rm \
     --net dev-net \
-    --name api0-1 \
-    --ip 172.28.0.13 \
-    --hostname api0-1.api0.default.svc.cluster.local \
+    --name testnode \
+    --ip 172.28.0.20 \
+    --hostname api0-0.api0.default.svc.cluster.local \
     --add-host api0-0.api0.default.svc.cluster.local:172.28.0.12 \
     --add-host api0-1.api0.default.svc.cluster.local:172.28.0.13 \
     --add-host api0-2.api0.default.svc.cluster.local:172.28.0.14 \
     -v `pwd`:/project \
-    -p 8001:8000 \
     -it \
     yhuangsh/dev-alpine-erlang:latest
